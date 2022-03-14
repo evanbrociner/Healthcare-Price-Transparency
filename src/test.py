004207dcd435d710a26bea0a5de1c84c6c00de61
evanbrociner <evanbrociner@gmail.com>
2022-03-14 17:23:51 -0400

    SQL_queries

--- src/test.py
+++ src/test.py
@@ -2,14 +2,25 @@
 from doltpy.cli.read import read_pandas_sql
 from doltpy.cli import Dolt, DoltException
 
+def hospitals_per_state(state):
+    query = '''SELECT state,COUNT(state) AS sum
+    FROM hospitals
+    GROUP BY state'''
+    repo = Dolt('/Users/evan.brociner/Desktop/Job_Hunting_Projects/hospital-price-transparency')
 
-query = '''SELECT state,COUNT(state) AS sum
-FROM hospitals
-GROUP BY state'''
-repo = Dolt('/Users/evan.brociner/Desktop/Job_Hunting_Projects/hospital-price-transparency')
+    result = read_pandas_sql(repo, query)
+    print(result)
 
-result = read_pandas_sql(repo, query)
-print(result)
-#
-#
-# tables = doltpy.dolt.ls()
+
+#def hospitals_per_state(state):
+
+
+def get_average_price():
+
+    query = '''SELECT AVG(price)
+    FROM prices
+    GROUP BY code,npi_number'''
+    repo = Dolt('/Users/evan.brociner/Desktop/Job_Hunting_Projects/hospital-price-transparency')
+
+    result = read_pandas_sql(repo, query)
+    print(result)
